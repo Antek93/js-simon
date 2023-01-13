dc7d698cbb5b5c8de9ff91e0acb6f35941c75dbe
Antek93 <antonio.savino1993@gmail.com>
2023-01-13 15:45:54 +0100

    creati numeri in pagina

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ console.log('HI, I am java and I am working fine');
 // 1.:  Creare una formula per generare 5 numeri casuali
 // 1.a: Creare un array per contenere i numeri casuali
 // 2.:  Stampa i numeri generati sulla pagina
-// 2.a  Creare cinque box per html
 // 3.:  Dopo 30 secondi:
 // 3.a: - i numeri scompaiono
 // 3.b: - l'utente deve inserire i numeri che si ricorda tramite prompt
@@ -25,7 +24,7 @@ console.log('Verifica numero casuale:', random, typeof random );
 
 let boxNumbers = [];
 
-// 2.a) Ciclo for per replicare la funzione numero casuale 5 volte
+// 2) Ciclo for per replicare la funzione numero casuale 5 volte
 
 for (let i = 0; i <= 5; i++) {
 
@@ -36,12 +35,36 @@ for (let i = 0; i <= 5; i++) {
 
 }
 
-// 2.b) Ciclo for per creare div contenitori
+// Manipolare il DOM per selezionare i box
 
-for (let i = 0; i <= 5; i++) {
+let boxUno = document.getElementById('First-box');
+let boxDue = document.getElementById('Second-box');
+let boxTre = document.getElementById('Third-box');
+let boxQuattro = document.getElementById('Fourth-box');
+let boxCinque = document.getElementById('Fifth-box');
 
-    document.createElement
+// Inserire i numeri nei box
 
-}
+boxUno.innerHTML = boxNumbers[0];
+boxDue.innerHTML = boxNumbers[1];
+boxTre.innerHTML = boxNumbers[2];
+boxQuattro.innerHTML = boxNumbers[3];
+boxCinque.innerHTML = boxNumbers[4];
+
+// Verifiche box
+
+console.log('Box uno:', boxUno)
+console.log('Box due:', boxDue)
+console.log('Box tre:', boxTre)
+console.log('Box quattro:', boxQuattro)
+console.log('Box cinque', boxCinque)
+
+// 3.)
+// 3.a) Dopo 30 secondi i numeri scompaiono
+
+
+
+
+
 
 
